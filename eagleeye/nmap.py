566a09dd6daec0027cf440b0f9bf99d51b585f7a
Paul McMillan <paul.mcmillan@nebula.com>
2013-04-18 00:00:18 -0400

    rework mostly settled

--- eagleeye/nmap.py
+++ eagleeye/nmap.py
@@ -7,20 +7,46 @@ from lxml import objectify
 from eagleeye import RedisWorker
 
 class NmapWorker(RedisWorker):
-    def set_read(self, value):
-        return self.redis.smembers(value)
+    qoutput = 'image:http'
+
+    def add_job(self, hosts, port_set):
+        queue = self.queue('verify:port:%s' % port_set)
+        self.redis.sadd('verify:port:set', port_set)
+        for host in hosts:
+            queue.send(host)
 
     def jobs(self):
         while True:
-            key_set = self.set_read('verify:port:set')
+            key_set = self.redis.smembers('verify:port:set')
             for key in key_set:
                 yield key
+            else:
+                # don't busy loop while we wait for initial set members
+                yield
 
-    def run(self, job):
-        result = []
-        for host in filter_open(job):
-            self.write('image:http', host)
-            result.append(host)
+    def run(self, ports):
+        hosts = self.finite_queue(job)[:1000]
+
+        for host in basic_nmap(hosts, ports):
+            # XXX better output queue selection
+            yield host
+
+
+def basic_nmap(hosts, ports):
+    command = ['nmap', '-T5', '--no-stylesheet', '-Pn', '-sT',
+               '--min-rate', '500', '--host-timeout', '15s',
+               '-n', # No DNS resolution
+               '-oX', '-',  # output XML to stdout
+               '-p', ports] + host_list
+    # This could be more efficient by not waiting for nmap to finish
+    # before starting XML parsing
+    nmap_xml_output = subprocess.check_output(command)
+    nmaprun = objectify.fromstring(nmap_xml_output)
+    for host in nmaprun.host:
+        for port in host.ports.port:
+            if port.state.get('state') == 'open':
+                yield dict(host=host.address.get('addr'),
+                           port=port.get('portid'))
 
 
 def filter_open(hosts):
--- eagleeye/worker.py
+++ eagleeye/worker.py
@@ -1,8 +1,10 @@
+import itertools
 import json
 
 import redis
 
-from eagleeye.utils import iterit, start_gen
+from eagleeye.utils import iterit
+from eagleeye.utils import start_gen
 
 # RPOP, LPUSH
 # NO!
@@ -26,8 +28,8 @@ class BaseWorker(object):
         """ Iterate through available jobs, handling each one.  Note
         that this construction leaves the blocking/non-blocking
         decision up to the job itself:
-         - if the job is finite, this handles each item once
-         - if the job is infinite, this yields None when the job is not true
+         - if jobs is finite, this handles each item once
+         - if jobs is infinite, this yields None when the job is not true
         This works because None is never a valid job.
         """
         for job in self.jobs(*args, **kwargs):
@@ -40,7 +42,9 @@ class RedisWorker(BaseWorker):
     qinput = None
     qoutput = None
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, qinput=None, qoutput=None, *args, **kwargs):
+        self.qinput = self.queue(qinput or self.qinput)
+        self.qoutput = self.queue(qoutput or self.qoutput)
         # This should become a shared pool once we have worker management
         self.redis = redis.StrictRedis(host='localhost', port=6379, db=0)
         return super(RedisWorker, self).__init__(*args, **kwargs)
@@ -58,26 +62,38 @@ class RedisWorker(BaseWorker):
             return json.loads(value)
 
     @start_gen
-    def queue(self, queue):
+    def queue(self, queue_name):
+        """ Create a queue object. Treat this like any other iterator,
+        except that it also supports the send(value) method in
+        addition to the standard next().
+
+        Note that queues continue to return None objects when they're
+        empty, so you can iterate forever over them.
+        """
         result = yield
         while True:
             while result:
                 result = (self.serialize(v) for v in result)
-                result = yield self.redis.write(queue, result)
-            result = yield self.deserialize(self.redis.lpop(queue))
+                result = yield self.redis.rpush(queue_name, *result)
+            result = yield self.deserialize(self.redis.lpop(queue_name))
+
+    def finite_queue(self, queue_name):
+        """ Use this for a queue that stops when there are no more
+        items rather than returning None forever.
+        """
+        return itertools.takewhile(bool, self.queue(queue_name))
 
     def jobs(self):
         """ Iterator that produces jobs to run in this worker.
 
         This returns None (a non-job) when there is nothing in the queue.
         """
-        while True:
-            yield self.read()
+        return queue(self.qinput)
 
     def handle(self, job):
         result = self.run(job)
         if result:
-            self.write(self.qoutput, *result)
+            self.qoutput.send(result)
 
     def run(self, job):
         """ The actual work of the class.
