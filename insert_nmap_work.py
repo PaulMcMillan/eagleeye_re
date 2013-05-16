7e44758d5c3c11001b0b552039172e7a6162c31b
Paul McMillan <paul.mcmillan@nebula.com>
2013-05-16 19:32:35 -0400

    silly strings

--- insert_nmap_work.py
+++ insert_nmap_work.py
@@ -13,9 +13,9 @@ worker = CommandWorker()
 
 for sub in ip.subnet(SUBNET_PREFIXLEN):
     cmd = ['nmap',  '-T4',  '--open',  '-sV',  '-sT',  '-sC',
-           '-A', '-O', '-n', '-Pn',  '--min-parallelism 50',
+           '-A', '-O', '-n', '-Pn',  '--min-parallelism', '50',
            '--min-rate',  '100',  '-g',  '53',
-           '--min-host-group', sub.prefixlen,
+           '--min-host-group', str(sub.prefixlen),
            '-oX', '-',
            str(sub)
            ]
